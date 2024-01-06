594361e6cfaf4d8314849661b995225a3bc84a88
Kota_Price <xkota1119x@gmail.com>
2024-01-06 09:16:28 +0900

    header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "./theme";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Play&family=Source+Sans+3&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <div>
+        <Component {...pageProps} />
+      </div>
+    </ThemeProvider>
+  );
 }
